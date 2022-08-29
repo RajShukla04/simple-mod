ab448d56917451d1d57bb9f5ecd6a64f752c3f6c
Shadow <98760996+Xdshadow01@users.noreply.github.com>
2022-08-29 17:00:38 +0530

    new upload

---
+++ hehe.js
@@ -0,0 +1,19 @@
+//console.log("hey")
+/*const hey = "lmao";
+const lmao =  prompt(Number("enter a no"))
+
+if (hey > lmao) {
+  console.log("big")
+}else{
+  console.log("little")
+}*/
+
+/*const lmao = (opps) => {
+  hehe = 0
+  for (let op of opps) {
+    hehe = (op)
+  }
+  return {hehe}
+}
+hey = lmao("7265282hzgsgwhus", "cacgwywrwyuwbwbw" , "svsyeywhwb")
+console.log(hey)*/
